b0ab299776441ca0bbd870568b264a75ca2dae65
KylieBeck <kylie.beck12@gmail.com>
2025-06-20 16:29:39 -0700

    dashboard css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -8,11 +8,12 @@ const Dashboard = () => {
     const { user } = useContext(UserContext);
     return (
         <main>
-            <h1>Welcome {user.username}!</h1>
-            <li><Link to='/students'>My Class List</Link></li>
-            <li><Link to='/assignments'>My Assignments</Link></li>
-            <li><Link to='/assignments/new'>New Assignment</Link></li>
-
+            <div className='dashboardBody'>
+                <h1>Welcome {user.username}!</h1>
+                <li><Link to='/students'>My Class List</Link></li>
+                <li><Link to='/assignments'>My Assignments</Link></li>
+                <li><Link to='/assignments/new'>New Assignment</Link></li>
+            </div>
         </main>
     );
 };
